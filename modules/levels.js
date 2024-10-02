e033595e52602506271e922f5789ebc5646a385d
Christine <christine-bui@hotmail.com>
2024-10-03 00:19:14 +0800

    reduce cache usage

--- modules/levels.js
+++ modules/levels.js
@@ -25,51 +25,62 @@
 //     requestAnimationFrame(draw);
 // }
 
-function level1(canvas,ctx){
-  var w1 = new GameObjectLocked(ctx,100,100,100,200);
-  var w2 = new GameObjectLocked(ctx,400,300,300,100);
-  let fixedAvatar = new AvatarFixed(canvas.width/2,canvas.height/2,avatarWidth,avatarHeight);
-  let moveableBG = new BGLocked("images/floor.png");
-  var item1 = new GameObjectInteractableLocked(ctx,300,50,20,20);
+// function level1(canvas,ctx){
+//   var w1 = new GameObjectLocked(ctx,100,100,100,200);
+//   var w2 = new GameObjectLocked(ctx,400,300,300,100);
+//   let fixedAvatar = new AvatarFixed(canvas.width/2,canvas.height/2,avatarWidth,avatarHeight);
+//   let moveableBG = new BGLocked("images/floor.png");
+//   var item1 = new GameObjectInteractableLocked(ctx,300,50,20,20);
+//   let img0 = new Image();
+//     img0.crossOrigin = "anonymous";
+// // img.src = `images/${imgName}`;
+//     img0.src = "images/bed.png";
+//     console.log("img:",img0.src);
+    
+//   //DRAW LOOP===========================
+//   function draw(){
+//       //menu();
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       //ctx.drawImage(background,0,0,1024,631,-bgX,-bgY,1024,631);//add this to BG class as well//DONT REMOVE YET
+
+//       //moveableBG.drawUpdate(ctx);
+//       moveableBG.drawPattern(ctx,tileObj);
+//       fixedAvatar.switchSprite(ctx,avatarX,avatarY);
+//       w1.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
+//       w2.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
+      
+      
+//       item1.drawObj_BGFixed(bgX,bgY);
+//       w1.drawObjImgLayer(ctx,img0, bgX,bgY,0,0,0,40);
+//       w1.collisionObj(bgX,bgY);
+//       w2.collisionObj(bgX,bgY);
+//       item1.collisionObj(bgX,bgY);
+//       //item1.pickup();
+//       item1.getEnergy();
+//       moveableBG.moveBG(canvas);
+     
+//      // fixedAvatar.usePower(ctx,avatarX,avatarY);
+//      fixedAvatar.usePowerColorStream(ctx,avatarX,avatarY,energy);
+//       openMenu(canvas,ctx);
+//       requestAnimationFrame(draw);
+      
+//   }//===================================
+//   //execute
+//   requestAnimationFrame(draw);//framerate 
+// }
+
   let img0 = new Image();
     img0.crossOrigin = "anonymous";
-// img.src = `images/${imgName}`;
-    img0.src = "images/bed.png";
-    console.log("img:",img0.src);
-    
-  //DRAW LOOP===========================
-  function draw(){
-      //menu();
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      //ctx.drawImage(background,0,0,1024,631,-bgX,-bgY,1024,631);//add this to BG class as well//DONT REMOVE YET
-
-      //moveableBG.drawUpdate(ctx);
-      moveableBG.drawPattern(ctx,tileObj);
-      fixedAvatar.switchSprite(ctx,avatarX,avatarY);
-      w1.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
-      w2.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
-      
-      
-      item1.drawObj_BGFixed(bgX,bgY);
-      w1.drawObjImgLayer(ctx,img0, bgX,bgY,0,0,0,40);
-      w1.collisionObj(bgX,bgY);
-      w2.collisionObj(bgX,bgY);
-      item1.collisionObj(bgX,bgY);
-      //item1.pickup();
-      item1.getEnergy();
-      moveableBG.moveBG(canvas);
-     
-     // fixedAvatar.usePower(ctx,avatarX,avatarY);
-     fixedAvatar.usePowerColorStream(ctx,avatarX,avatarY,energy);
-      openMenu(canvas,ctx);
-      requestAnimationFrame(draw);
-      
-  }//===================================
-  //execute
-  requestAnimationFrame(draw);//framerate 
-}
-
-
+    img0.src = "images/buildingLong.png";
+    wallImg = new Image();
+    wallImg.crossOrigin = "anonymous";
+    wallImg.src = "images/wall.png";
+    let boxImg = new Image();
+    boxImg.crossOrigin = "anonymous";
+    boxImg.src = "images/box.png";
+    let whImg = new Image();
+    whImg.crossOrigin = "anonymous";
+    whImg.src = "images/warehouse.png";
 
 function level2(canvas,ctx){
  
@@ -83,19 +94,7 @@ function level2(canvas,ctx){
   var item1 = new GameObjectInteractableLocked(ctx,300,50,20,20);
   var box = new GameObjectInteractableLocked(ctx,200,600,30,30);
   var enemy = new Enemy(ctx,0,0,30,30);
-    let img0 = new Image();
-    img0.crossOrigin = "anonymous";
-    img0.src = "images/buildingLong.png";
-    wallImg = new Image();
-    wallImg.crossOrigin = "anonymous";
-    wallImg.src = "images/wall.png";
-    let boxImg = new Image();
-    boxImg.crossOrigin = "anonymous";
-    boxImg.src = "images/box.png";
-
-    let whImg = new Image();
-    whImg.crossOrigin = "anonymous";
-    whImg.src = "images/warehouse.png";
+    
   //DRAW LOOP===========================
   function draw(){
       //menu();
