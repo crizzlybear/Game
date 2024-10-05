3dbe16851780f0a8d9c308179ffa2fde269830e9
Christine <christine-bui@hotmail.com>
2024-10-05 23:26:33 +0800

    cleanup comments, moved images to be loaded to levels

--- modules/globalVariables.js
+++ modules/globalVariables.js
@@ -1,23 +1,3 @@
-// let avatarX = 10;
-// let avatarY = 10;
-// let avatarWidth = 50;//20
-// let avatarHeight = 50;
-// let speed = 2;
-
-
-//sprite
-let n=0;
-let last = 0;//should be private
-let av = new Image();
-//av.src = "images/dogSample.png";
-av.src = "images/dogSample4.png";
-// let avH = 128;
-// let avW = 455;
-// let avDiv = 455/4;
-
-// let avH = 100;
-// let avW = 400;
-// let avDiv = 100;
 let avH = 100;
 let avW = av.width;
 let avDiv = 100;
@@ -32,14 +12,6 @@ let escPressed = false;
 let count=0;
 menu = false;
 menuOpen = false;
-//bg
-let background = new Image();
-// let bgX = 0;
-// let bgY = 0;
-
-//gameobj
-// let ob = new Image();
-// ob.src = "images/bed.png";
 
 
 let powerOn =false;
@@ -49,5 +21,3 @@ let rgb = { r: 0, g: 0, b: 0 };
 let itemQueue = [];
 let energy = 1000;
 
-let tileObj = new Image();
-tileObj.src = "images/floortile.png";
--- modules/levels.js
+++ modules/levels.js
@@ -1,86 +1,23 @@
-// function level1(canvas,ctx){
-//     var w1 = new GameObject(ctx,20,20,100,200);
-//     var item = new GameObjectInteractable(ctx,100,100,20,20);
-//     var myAvatar = new Avatar(avatarWidth,avatarHeight);
- 
-//     var bg1 = new BG("images/floor.png");
-//     avatarX = canvas.width/2;//init
-//     avatarY = 40;
- 
-//     //DRAW LOOP===========================
-//     function draw(){
-//       ctx.clearRect(0, 0, canvas.width, canvas.height);
-//       bg1.drawBG(canvas,ctx);
-//       myAvatar.drawAvatar(ctx,avatarX,avatarY);
-//       myAvatar.switchSprite(ctx,avatarX,avatarY);
-//       w1.createObj();
-//       w1.drawImg();
-//       item.createInteractable();
-//       myAvatar.moveBoundaries(canvas);
-      
-//       openMenu(canvas,ctx);
-//       requestAnimationFrame(draw);
-//     }//===================================
-//     //execute
-//     requestAnimationFrame(draw);
-// }
-
-// function level1(canvas,ctx){
-//   var w1 = new GameObjectLocked(ctx,100,100,100,200);
-//   var w2 = new GameObjectLocked(ctx,400,300,300,100);
-//   let fixedAvatar = new AvatarFixed(canvas.width/2,canvas.height/2,avatarWidth,avatarHeight);
-//   let moveableBG = new BGLocked("images/floor.png");
-//   var item1 = new GameObjectInteractableLocked(ctx,300,50,20,20);
-//   let img0 = new Image();
-//     img0.crossOrigin = "anonymous";
-// // img.src = `images/${imgName}`;
-//     img0.src = "images/bed.png";
-//     console.log("img:",img0.src);
-    
-//   //DRAW LOOP===========================
-//   function draw(){
-//       //menu();
-//       ctx.clearRect(0, 0, canvas.width, canvas.height);
-//       //ctx.drawImage(background,0,0,1024,631,-bgX,-bgY,1024,631);//add this to BG class as well//DONT REMOVE YET
-
-//       //moveableBG.drawUpdate(ctx);
-//       moveableBG.drawPattern(ctx,tileObj);
-//       fixedAvatar.switchSprite(ctx,avatarX,avatarY);
-//       w1.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
-//       w2.drawObj_BGFixed(bgX,bgY);//make this before moveBG so theres no lag
-      
-      
-//       item1.drawObj_BGFixed(bgX,bgY);
-//       w1.drawObjImgLayer(ctx,img0, bgX,bgY,0,0,0,40);
-//       w1.collisionObj(bgX,bgY);
-//       w2.collisionObj(bgX,bgY);
-//       item1.collisionObj(bgX,bgY);
-//       //item1.pickup();
-//       item1.getEnergy();
-//       moveableBG.moveBG(canvas);
-     
-//      // fixedAvatar.usePower(ctx,avatarX,avatarY);
-//      fixedAvatar.usePowerColorStream(ctx,avatarX,avatarY,energy);
-//       openMenu(canvas,ctx);
-//       requestAnimationFrame(draw);
-      
-//   }//===================================
-//   //execute
-//   requestAnimationFrame(draw);//framerate 
-// }
-
-  let img0 = new Image();
-    img0.crossOrigin = "anonymous";
-    img0.src = "images/buildingLong.png";
+    //Preload images =======================================
+    //bg
+    let background = new Image();
+    let tileObj = new Image();
+      tileObj.src = "images/floortile.png";
+    //sprite
+    let av = new Image();
+      av.src = "images/dogSample4.png";
+    let img0 = new Image();
+      img0.crossOrigin = "anonymous";
+      img0.src = "images/buildingLong.png";
     wallImg = new Image();
-    wallImg.crossOrigin = "anonymous";
-    wallImg.src = "images/wall.png";
+      wallImg.crossOrigin = "anonymous";
+      wallImg.src = "images/wall.png";
     let boxImg = new Image();
-    boxImg.crossOrigin = "anonymous";
-    boxImg.src = "images/box.png";
+      boxImg.crossOrigin = "anonymous";
+      boxImg.src = "images/box.png";
     let whImg = new Image();
-    whImg.crossOrigin = "anonymous";
-    whImg.src = "images/warehouse.png";
+      whImg.crossOrigin = "anonymous";
+     whImg.src = "images/warehouse.png";
 
 function level2(canvas,ctx){
  
