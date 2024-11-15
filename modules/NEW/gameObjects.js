56bbdd02230cdba5bdf817a66cb000750f9677e1
Christine <christine-bui@hotmail.com>
2024-11-15 13:10:35 +0800

    get energy show item when selected

--- modules/NEW/gameObjects.js
+++ modules/NEW/gameObjects.js
@@ -74,6 +74,7 @@ class GameObjectInteractableLocked extends GameObjectLocked{
     this.key = key;
     this.visible=true;
     this.frameW=0;
+    this.energy=500;
   }
   setNewRGBandColor(){
     var rgbList = [[255,0,0],[0,255,0],[0,0,255],[0,0,0]];
@@ -136,14 +137,20 @@ class GameObjectInteractableLocked extends GameObjectLocked{
     let botBorder=this.y+this.h + 10;
     let randomPositions = [[0,50],[400,80],[380,220],[200,510],[300,550]];
     if((pickup) && ((rightBorder-bgInst.bgX>avatarInst.x)&&(botBorder-bgInst.bgY>avatarInst.y)&&(leftBorder-bgInst.bgX<avatarInst.x+ avatarInst.w)&&(topBorder-bgInst.bgY<avatarInst.y+ avatarInst.h))){
-      avatarInst.energy=1000;
+      this.energy-=10;
+      avatarInst.energy = Math.min(avatarInst.energy+10,1000);
+
       //this.col = "#808080";
       this.col = "#71ffff";
       this.frameW=30;
-      var coord = Math.round(Math.random()*(randomPositions.length-1));
-      console.log(coord);
-      this.x = randomPositions[coord][0];
-      this.y=randomPositions[coord][1];
+      
+      if(this.energy==0){
+        var coord = Math.round(Math.random()*(randomPositions.length-1));
+        this.x = randomPositions[coord][0];
+        this.y=randomPositions[coord][1];
+        this.energy=500;
+      }
+      
     }else{
       // this.frameW=30;
       this.frameW=0;
